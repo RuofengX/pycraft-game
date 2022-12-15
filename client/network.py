7de7695953497c56acfad06caef1327acf09ee49
ruofeng <weiruofeng@ruofengx.cn>
2022-12-15 22:45:31 +0800

    Client now inherit ClientSession and support `async with` sentence.

--- client/network.py
+++ client/network.py
@@ -1,8 +1,10 @@
 from dataclasses import dataclass
+from typing import Any, Dict, NoReturn, Optional
+
 from aiohttp import ClientSession
-from typing import Dict, Any, Optional, NoReturn
 
 from pyworld.datamodels.function_call import ServerRtn
+
 # from pyworld.datamodels.websockets import WSPayload
 
 
@@ -22,12 +24,13 @@ class Credential():
     password: Optional[str] = None
 
 
-class Client():
+class Client(ClientSession):
+    """Use async with Client() to Open new session."""
 
     def __init__(
         self,
         cred: Optional[Credential] = None,
-    ):
+    ) -> None:
         if cred is not None:
             self.username: Optional[str] = cred.username
             self.password: Optional[str] = cred.password
@@ -35,9 +38,8 @@ class Client():
     async def register(self, username, password) -> NoReturn | Dict[str, Any]:
         uri = f'{PROTOCOL}{BASE_URL}{REGISTER_PATH}'\
             '?username={username}&password={password}'
-        async with ClientSession() as cs:
-            async with cs.get(url=uri) as response:
-                assert response.status == 200, response.status
-                return ServerRtn(
-                    **(await response.json())
-                ).dict()
+        async with self.get(url=uri) as response:
+            assert response.status == 200, response.status
+            return ServerRtn(
+                **(await response.json())
+            ).dict()
